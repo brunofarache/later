1f4bdc302e37c33999cf2e2fb30ff11c8e08e0ad
Victor Galan <victor.galan@liferay.com>
2016-11-24 16:16:45 +0100

    Migrate to swift3 AllOperation class

--- Source/Operations/AllOperation.swift
+++ Source/Operations/AllOperation.swift
@@ -9,20 +9,19 @@ class AllOperation : Operation {
 	}
 
 	override func main() {
-		var results = [Any?](count: self.blocks.count, repeatedValue: nil)
+		var results = [Any?](repeating: nil, count: self.blocks.count)
 		let operation = dependencies.last as? Operation
 
-		let group = dispatch_group_create()
-		let queue = dispatch_get_global_queue(
-			DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+		let group = DispatchGroup()
+		let queue = DispatchQueue.global()
 
-		for (i, block) in blocks.enumerate() {
-			dispatch_group_async(group, queue, { () -> () in
+		for (i, block) in blocks.enumerated() {
+			queue.async(group: group) {
 				results[i] = block(operation?.output)
-			})
+			}
 		}
 
-		dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+		group.wait()
 
 		output = results
 	}
